ac63cc02b2246fe77167dc12bc89113e7edc44d4
kakcruz <karolinecsantos@hotmail.com>
2019-08-26 22:06:50 -0300

    Criei um app da Birpo

---
+++ app/src/main/java/br/com/etecia/appbirpo/login_layout.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appbirpo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
